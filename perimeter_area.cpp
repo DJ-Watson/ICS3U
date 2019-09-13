03cdd4ad3094eb87cd3ebb7699b0a2e6caa70607
Ubuntu <ubuntu@ip-172-31-28-83.ec2.internal>
2019-09-13 17:27:44 +0000

    added unit 1-04

---
+++ perimeter_area.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 DJ Watson All Rights Reserved
+//
+// Created by DJ Watson
+// Created on September 2019
+
+#include <iostream>
+
+int main(){
+	std::cout << "if a rectangle has the dimensions" << std::endl;
+	std::cont << "5mm x 3mm" << std::endl;
+	std::cont << "" << std::endl;
+	std::cont << "AREA:" << (5*3) << "mm2" <<std::endl;
+	std::cont << "PERIMETER" << ((5+3)*2) << "mm" <<std::endl;
+}
